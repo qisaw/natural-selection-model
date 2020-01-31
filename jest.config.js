7b0b54824c80c0ba7690fa7f985db010d6c7c85c
Wasiq Kashkari <w.kashkari@outlook.com>
2020-01-31 23:24:02 +0100

    Set up typescript and jest

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  collectCoverage: true,
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+   //An array of regexp pattern strings used to skip coverage collection
+   coveragePathIgnorePatterns: [
+     "/node_modules/"
+   ],
+
+   moduleFileExtensions: [
+     "js",
+     "json",
+     "jsx",
+     "ts",
+     "tsx",
+     "node"
+   ],
+
+  testMatch: [
+    "**/src/**/*.test.ts?(x)",
+  ],
+};
---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+describe('First Test', () => {
+  it('should be ok', () => {
+    expect(true).toEqual(true)
+  })
+})
