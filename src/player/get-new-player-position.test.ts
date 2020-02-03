4ff763567e9d7ecd77b64f216a26a9c2336f1f75
Wasiq Kashkari <w.kashkari@outlook.com>
2020-02-03 22:10:22 +0100

    Avoid collisions

--- src/player/get-new-player-position.test.ts
+++ src/player/get-new-player-position.test.ts
@@ -2,27 +2,22 @@ import { createPlayer } from '.';
 import { createGround } from '../ground/create-ground';
 import { getNewPlayerPosition } from './get-new-player-position';
 
+const dimensions = {
+  width: 10,
+  height: 10,
+};
 describe('getNewPlayerPosition', () => {
-  const players = [createPlayer({ position: { x: 1, y: 1 } })];
-  const dimensions = {
-    width: 10,
-    height: 10,
-  };
-  const ground = createGround({
-    dimensions,
-    players,
-  });
   describe('happy paths', () => {
-    it('should return position', () => {
-      const newDimensions = getNewPlayerPosition(players[0], ground);
-      expect(newDimensions.x).toBeDefined();
-      expect(newDimensions.y).toBeDefined();
+    afterEach(() => {
+      jest.restoreAllMocks();
     });
     describe('moving directions', () => {
-      afterEach(() => {
-        jest.restoreAllMocks();
-      });
       describe('when all directions are available', () => {
+        const players = [createPlayer({ position: { x: 1, y: 1 } })];
+        const ground = createGround({
+          dimensions,
+          players,
+        });
         it('should move the user up 1/4th of the time', () => {
           jest.spyOn(Math, 'random').mockReturnValue(0.24);
           const playerToMove = players[0];
@@ -54,6 +49,10 @@ describe('getNewPlayerPosition', () => {
       });
       describe('when the user is in the top row', () => {
         const playersOnTopRow = [createPlayer({ position: { x: 1, y: 0 } })];
+        const ground = createGround({
+          dimensions,
+          players: playersOnTopRow,
+        });
         it('should move the user down 1/3 of the time', () => {
           jest.spyOn(Math, 'random').mockReturnValue(0.32);
           const playerToMove = playersOnTopRow[0];
@@ -78,6 +77,10 @@ describe('getNewPlayerPosition', () => {
       });
       describe('when the user is in the left most row', () => {
         const playersLeftMost = [createPlayer({ position: { x: 0, y: 1 } })];
+        const ground = createGround({
+          dimensions,
+          players: playersLeftMost,
+        });
         it('should move the user up 1/3 of the time', () => {
           jest.spyOn(Math, 'random').mockReturnValue(0.32);
           const playerToMove = playersLeftMost[0];
@@ -102,6 +105,10 @@ describe('getNewPlayerPosition', () => {
       });
       describe('when the user is in the bottom most row', () => {
         const playersBottom = [createPlayer({ position: { x: 1, y: dimensions.height - 1 } })];
+        const ground = createGround({
+          dimensions,
+          players: playersBottom,
+        });
         it('should move the user up 1/3 of the time', () => {
           jest.spyOn(Math, 'random').mockReturnValue(0.32);
           const playerToMove = playersBottom[0];
@@ -126,6 +133,10 @@ describe('getNewPlayerPosition', () => {
       });
       describe('when the user is in the right most row', () => {
         const playersRightMost = [createPlayer({ position: { x: dimensions.width - 1, y: 1 } })];
+        const ground = createGround({
+          dimensions,
+          players: playersRightMost,
+        });
         it('should move the user up 1/3 of the time', () => {
           jest.spyOn(Math, 'random').mockReturnValue(0.32);
           const playerToMove = playersRightMost[0];
@@ -150,6 +161,10 @@ describe('getNewPlayerPosition', () => {
       });
       describe('when the user is in the top left corner', () => {
         const playersTopLeftCorner = [createPlayer({ position: { x: 0, y: 0 } })];
+        const ground = createGround({
+          dimensions,
+          players: playersTopLeftCorner,
+        });
         it('should move the user down 1/2 of the time', () => {
           jest.spyOn(Math, 'random').mockReturnValue(0.49);
           const playerToMove = playersTopLeftCorner[0];
@@ -167,6 +182,10 @@ describe('getNewPlayerPosition', () => {
       });
       describe('when the user is in the top right corner', () => {
         const playersTopRightCorner = [createPlayer({ position: { x: dimensions.width - 1, y: 0 } })];
+        const ground = createGround({
+          dimensions,
+          players: playersTopRightCorner,
+        });
         it('should move the user down 1/2 of the time', () => {
           jest.spyOn(Math, 'random').mockReturnValue(0.49);
           const playerToMove = playersTopRightCorner[0];
@@ -184,6 +203,10 @@ describe('getNewPlayerPosition', () => {
       });
       describe('when the user is in the bottom left corner', () => {
         const playersBottomLeftCorner = [createPlayer({ position: { x: 0, y: dimensions.height - 1 } })];
+        const ground = createGround({
+          dimensions,
+          players: playersBottomLeftCorner,
+        });
         it('should move the user up 1/2 of the time', () => {
           jest.spyOn(Math, 'random').mockReturnValue(0.49);
           const playerToMove = playersBottomLeftCorner[0];
@@ -200,23 +223,81 @@ describe('getNewPlayerPosition', () => {
         });
       });
       describe('when the user is in the bottom right corner', () => {
-        const playersRightLeftCorner = [
+        const playersBottomRightCorner = [
           createPlayer({ position: { x: dimensions.width - 1, y: dimensions.height - 1 } }),
         ];
+        const ground = createGround({
+          dimensions,
+          players: playersBottomRightCorner,
+        });
         it('should move the user up 1/2 of the time', () => {
           jest.spyOn(Math, 'random').mockReturnValue(0.49);
-          const playerToMove = playersRightLeftCorner[0];
+          const playerToMove = playersBottomRightCorner[0];
           const position = getNewPlayerPosition(playerToMove, ground);
           expect(position.x).toEqual(playerToMove.position.x);
           expect(position.y).toEqual(playerToMove.position.y - 1);
         });
         it('should move the user left 1/2 of the time', () => {
           jest.spyOn(Math, 'random').mockReturnValue(0.99);
-          const playerToMove = playersRightLeftCorner[0];
+          const playerToMove = playersBottomRightCorner[0];
           const position = getNewPlayerPosition(playerToMove, ground);
           expect(position.x).toEqual(playerToMove.position.x - 1);
           expect(position.y).toEqual(playerToMove.position.y);
         });
+      });
+    });
+    describe('collisions', () => {
+      it('should not move to a position where there is another player', () => {
+        /*
+         * | - | x | - |
+         * | x | y | - |
+         * | - | x | - |
+         * we want to move y
+         * the only available move for y is right one
+         */
+        const players = [
+          createPlayer({ position: { x: 0, y: 1 } }),
+          createPlayer({ position: { x: 1, y: 0 } }),
+          createPlayer({ position: { x: 1, y: 2 } }),
+          createPlayer({ position: { x: 1, y: 1 } }),
+        ];
+        const ground = createGround({
+          dimensions,
+          players,
+        });
+        const playerToMove = players[3];
+        const mathRandomMock = jest.spyOn(Math, 'random');
+        mathRandomMock.mockReturnValue(0);
+        expect(getNewPlayerPosition(playerToMove, ground).x).toEqual(playerToMove.position.x + 1);
+        expect(getNewPlayerPosition(playerToMove, ground).y).toEqual(playerToMove.position.y);
+        mathRandomMock.mockReturnValue(0.5);
+        expect(getNewPlayerPosition(playerToMove, ground).x).toEqual(playerToMove.position.x + 1);
+        expect(getNewPlayerPosition(playerToMove, ground).y).toEqual(playerToMove.position.y);
+        mathRandomMock.mockReturnValue(1);
+        expect(getNewPlayerPosition(playerToMove, ground).x).toEqual(playerToMove.position.x + 1);
+        expect(getNewPlayerPosition(playerToMove, ground).y).toEqual(playerToMove.position.y);
+      });
+      it('should not make any moves if a player is totally surrounded', () => {
+        /*
+         * | - | x | - |
+         * | x | y | x |
+         * | - | x | - |
+         * we want to move y
+         * the only available move for y is right one
+         */
+        const players = [
+          createPlayer({ position: { x: 0, y: 1 } }),
+          createPlayer({ position: { x: 1, y: 0 } }),
+          createPlayer({ position: { x: 1, y: 2 } }),
+          createPlayer({ position: { x: 2, y: 1 } }),
+          createPlayer({ position: { x: 1, y: 1 } }),
+        ];
+        const playerToMove = players[4];
+        const ground = createGround({
+          dimensions,
+          players,
+        });
+        expect(getNewPlayerPosition(playerToMove, ground)).toEqual(playerToMove.position);
       });
     });
   });
--- src/player/get-new-player-position.ts
+++ src/player/get-new-player-position.ts
@@ -9,21 +9,63 @@ enum Direction {
   RIGHT,
 }
 
-const getRandomDirection = (availableDirections: Direction[]): Direction => {
+const getRandomDirection = (availableDirections: Direction[]): Direction | void => {
+  if (!availableDirections.length) {
+    return;
+  }
   const sparator = 1 / availableDirections.length;
   const randomValue = Math.random();
-  const index = Math.ceil(randomValue / sparator) - 1;
+  const index = Math.max(Math.ceil(randomValue / sparator) - 1, 0);
   return availableDirections[index];
 };
 
-const getDirection = (xValue: number, yValue: number, ground: Ground): Direction => {
+const isPositionFree = (xValue: number, yValue: number, direction: Direction, ground: Ground): boolean => {
+  let nextPosition: Position;
+  if (direction === Direction.UP) {
+    nextPosition = {
+      x: xValue,
+      y: yValue - 1,
+    };
+  } else if (direction === Direction.DOWN) {
+    nextPosition = {
+      x: xValue,
+      y: yValue + 1,
+    };
+  } else if (direction === Direction.LEFT) {
+    nextPosition = {
+      x: xValue - 1,
+      y: yValue,
+    };
+  } else {
+    nextPosition = {
+      x: xValue + 1,
+      y: yValue,
+    };
+  }
+  if (
+    nextPosition.x > ground.dimensions.width - 1 ||
+    nextPosition.x < 0 ||
+    nextPosition.y > ground.dimensions.height - 1 ||
+    nextPosition.y < 0
+  ) {
+    return false;
+  }
+  const playerAtPosition = ground.players.find(
+    ({ position }) => position.x === nextPosition.x && position.y === nextPosition.y,
+  );
+  return !playerAtPosition;
+};
+
+const getDirection = (xValue: number, yValue: number, ground: Ground): Direction | void => {
   const directionArray = [
     { value: Direction.UP, predicate: yValue > 0 },
     { value: Direction.DOWN, predicate: yValue < ground.dimensions.height - 1 },
     { value: Direction.LEFT, predicate: xValue > 0 },
     { value: Direction.RIGHT, predicate: xValue < ground.dimensions.width - 1 },
   ];
-  const availableDirections = directionArray.filter(({ predicate }) => predicate).map(({ value }) => value);
+  const availableDirections = directionArray
+    .filter(({ predicate, value }) => predicate && isPositionFree(xValue, yValue, value, ground))
+    .map(({ value }) => value);
   return getRandomDirection(availableDirections);
 };
 
@@ -49,6 +91,10 @@ export const getNewPlayerPosition = (player: Player, ground: Ground): Position =
       return {
         x: player.position.x + 1,
         y: player.position.y,
+      };
+    default:
+      return {
+        ...player.position,
       };
   }
 };
