ce0b4c32e6d6bc5390d280749b650acbf3aafacb
Wasiq Kashkari <w.kashkari@outlook.com>
2020-02-03 01:07:27 +0100

    In the main program, run 10 turns

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { createGround } from './ground/create-ground';
 import { printGround } from './ground/print-ground';
 import { createPlayer } from './player';
+import { makeMove } from './ground/make-move';
 
 const width = 10;
 const height = 10;
@@ -10,9 +11,15 @@ const players = [
   createPlayer({ position: { x: 0, y: 2 } }),
   createPlayer({ position: { x: 1, y: 3 } }),
 ];
-const ground = createGround({
+const initialGround = createGround({
   dimensions: { width, height },
   players,
 });
 
+let ground = initialGround;
 printGround(ground);
+
+for (let i = 0; i < 10; i++) {
+  ground = makeMove(ground);
+  printGround(ground);
+}
