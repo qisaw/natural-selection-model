25ca6ef91517ca0509a6660fa197d606b3e407b9
Wasiq Kashkari <w.kashkari@outlook.com>
2020-02-04 21:29:33 +0100

    Fix tests that I acciendally broke in my previous commit

--- src/ground/make-move.ts
+++ src/ground/make-move.ts
@@ -16,19 +16,19 @@ export const makeMove = (ground: Ground): Ground => {
   // the player earlier in the array will always get the food first.
   // We'd want to sort the array in a random manner each call of makeMove
   const newGround = players.reduce((newGround, player, idx) => {
-    if (player.energyAddition === 0) {
+    if (player.energy === 0) {
       return newGround;
     }
     const newPosition = getNewPlayerPosition(player, newGround);
     // @TODO ensure we don't change the energy if the player didn't move
-    const newPlayer = createPlayer({ ...player, position: newPosition, energy: player.energyAddition - 1 });
+    const newPlayer = createPlayer({ ...player, position: newPosition, energy: player.energy - 1 });
     const newPlayerArray = [...newGround.players.slice(0, idx), newPlayer, ...newGround.players.slice(idx + 1)];
     return { ...newGround, players: newPlayerArray };
   }, ground);
   // @TODO be careful when implementing eating that we don't have a dead person eaten more than once
   // We might need to refactor the order of this when we do this. But for now it works and is super simple.
   const { playersDeadThisTurn, playersStillAlive } = groupBy(newGround.players, player =>
-    player.energyAddition === 0 ? 'playersDeadThisTurn' : 'playersStillAlive',
+    player.energy === 0 ? 'playersDeadThisTurn' : 'playersStillAlive',
   ) as groupedPlayers;
   return {
     ...newGround,
