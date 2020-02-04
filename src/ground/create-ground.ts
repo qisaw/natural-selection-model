228395b0c5e06933de4a546cd54a38b37886dd0d
Wasiq Kashkari <w.kashkari@outlook.com>
2020-02-04 21:34:49 +0100

    Add food to ground

--- src/ground/create-ground.ts
+++ src/ground/create-ground.ts
@@ -4,6 +4,7 @@ import { Player } from '../player/player';
 import { InvalidDimensionsError } from './errors';
 import { PlayersOutsideGridError } from '../player/errors';
 import { GroundCreation, Ground } from './types';
+import { Food } from '../food/food';
 
 const defaultDimensions = {
   height: 10,
@@ -11,7 +12,13 @@ const defaultDimensions = {
 };
 const defaultPlayers: Player[] = [];
 
-export const createGround = ({ dimensions = defaultDimensions, players = defaultPlayers }: GroundCreation): Ground => {
+const defaultFood: Food[] = [];
+
+export const createGround = ({
+  dimensions = defaultDimensions,
+  players = defaultPlayers,
+  food = defaultFood,
+}: GroundCreation): Ground => {
   if (dimensions.height < 2 || dimensions.width < 2) {
     throw new InvalidDimensionsError(dimensions);
   }
@@ -26,6 +33,7 @@ export const createGround = ({ dimensions = defaultDimensions, players = default
     dimensions,
     players,
     id,
+    food,
     playersDeadThisTurn: [],
   };
 };
--- src/ground/types.ts
+++ src/ground/types.ts
@@ -1,4 +1,5 @@
 import { Player } from '../player/player';
+import { Food } from '../food/food';
 
 export interface GroundDimensions {
   width: number;
@@ -8,11 +9,13 @@ export interface GroundDimensions {
 export interface GroundCreation {
   dimensions?: GroundDimensions;
   players?: Player[];
+  food?: Food[];
 }
 
 export interface Ground {
   dimensions: GroundDimensions;
   players: Player[];
   playersDeadThisTurn: Player[];
+  food: Food[];
   id: string;
 }
