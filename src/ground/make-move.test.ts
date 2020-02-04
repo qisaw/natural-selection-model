f3170f6f952941a7a94f357232f7777d8930a5e3
Wasiq Kashkari <w.kashkari@outlook.com>
2020-02-04 21:32:20 +0100

    Remove only that I accidentally added

--- src/ground/make-move.test.ts
+++ src/ground/make-move.test.ts
@@ -57,7 +57,7 @@ describe('makeMove', () => {
       },
     ]);
   });
-  it.only('should put into the dead players array if their energy has been depleted', () => {
+  it('should put into the dead players array if their energy has been depleted', () => {
     const playersNoEnergy = [createPlayer({ position: { x: 1, y: 1 }, energy: 0 })];
     const ground = createGround({ dimensions, players: playersNoEnergy });
     expect(makeMove(ground)).toEqual({ ...ground, players: [], playersDeadThisTurn: playersNoEnergy });
