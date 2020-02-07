77b19018e95817bfb1ad65951db9669c946d6236
Wasiq Kashkari <w.kashkari@outlook.com>
2020-02-07 21:53:34 +0100

    Implement food eating
    When a player moves to a position where food exists, it eats the food. The
    food is then not available to be eaten again by any other player. Also
    the player gets an energyAddition equal to the energyAddition value of
    the food it just ate.

--- src/player/move-player.test.ts
+++ src/player/move-player.test.ts
@@ -2,6 +2,7 @@ import { createPlayer } from '.';
 import { createGround } from '../ground/create-ground';
 import { movePlayer } from './move-player';
 import { PlayerNotInGroundError } from '../ground/errors';
+import { createFood } from '../food';
 
 describe('movePlayer', () => {
   describe('the player has no energy', () => {
@@ -41,7 +42,39 @@ describe('movePlayer', () => {
         expect(newGround.players[i].energy).toEqual(1);
       }
     });
+    it('should not remove food not eaten', () => {
+      const players = [createPlayer({ position: { x: 1, y: 1 }, energy: 1 })];
+      const food = [createFood({ position: { x: 9, y: 9 } })];
+      const dimensions = { width: 10, height: 10 };
+      const ground = createGround({ players, food, dimensions });
+      expect(movePlayer(players[0], ground).food).toEqual(food);
+    });
+    it('should remove food when eaten', () => {
+      // here the first player only has one available direction (UP) to where the food is
+      const players = [
+        createPlayer({ position: { x: 1, y: 1 }, energy: 1 }),
+        createPlayer({ position: { x: 0, y: 1 }, energy: 1 }),
+        createPlayer({ position: { x: 0, y: 0 }, energy: 1 }),
+      ];
+      const food = [createFood({ position: { x: 1, y: 0 } })];
+      const dimensions = { width: 2, height: 2 };
+      const ground = createGround({ players, food, dimensions });
+      expect(movePlayer(players[0], ground).food).toHaveLength(0);
+    });
+    it('should add energy addition to player when food eaten', () => {
+      // here the first player only has one available direction (UP) to where the food is
+      const players = [
+        createPlayer({ position: { x: 1, y: 1 }, energy: 1 }),
+        createPlayer({ position: { x: 0, y: 1 }, energy: 1 }),
+        createPlayer({ position: { x: 0, y: 0 }, energy: 1 }),
+      ];
+      const food = [createFood({ position: { x: 1, y: 0 }, energyAddition: 10 })];
+      const dimensions = { width: 2, height: 2 };
+      const ground = createGround({ players, food, dimensions });
+      expect(movePlayer(players[0], ground).players[0].energy).toEqual(10);
+    });
   });
+
   describe('unhappy paths', () => {
     it('should throw PlayerNotInGroundError error if a player which is not on the ground', () => {
       const someRandomPlayer = createPlayer({ position: { x: 0, y: 0 }, energy: 1 });
--- src/player/move-player.ts
+++ src/player/move-player.ts
@@ -3,6 +3,7 @@ import { Ground } from '../ground/types';
 import { createPlayer } from '.';
 import { getNewPlayerPosition } from './get-new-player-position';
 import { PlayerNotInGroundError } from '../ground/errors';
+import { getFoodFromGround } from '../food/get-food-from-ground';
 
 export const movePlayer = (player: Player, ground: Ground): Ground => {
   const idx = ground.players.findIndex(({ id }: Player): boolean => id === player.id);
@@ -13,12 +14,19 @@ export const movePlayer = (player: Player, ground: Ground): Ground => {
     return { ...ground, players: ground.players.filter(({ id }: Player): boolean => id !== player.id) };
   }
   const newPosition = getNewPlayerPosition(player, ground);
-  const newEnergy =
+  let newEnergy =
     newPosition.x === player.position.x && newPosition.y === player.position.y ? player.energy : player.energy - 1;
+  const food = getFoodFromGround(ground, newPosition);
+  let updatedFood = ground.food;
+  if (food) {
+    updatedFood = updatedFood.filter(({ id }) => id !== food.id);
+    newEnergy += food.energyAddition;
+  }
   const newPlayer = createPlayer({ ...player, position: newPosition, energy: newEnergy });
   const newPlayerArray = [...ground.players.slice(0, idx), newPlayer, ...ground.players.slice(idx + 1)];
   return {
     ...ground,
+    food: updatedFood,
     players: newPlayerArray,
   };
 };
