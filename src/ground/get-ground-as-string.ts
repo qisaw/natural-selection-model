81e0d24a2ebaed842b3d147ea38e1e55fad4d93c
Wasiq Kashkari <w.kashkari@outlook.com>
2020-02-07 22:10:44 +0100

    Clean up finding players and food items in rows and columns
    The predicate function for these two are always the same.

--- src/ground/get-ground-as-string.ts
+++ src/ground/get-ground-as-string.ts
@@ -1,19 +1,26 @@
 import { Ground } from './types';
+import { Position } from '../global/types';
+
+type HasPosition = {
+  position: Position;
+};
 
 export const getGroundAsString = (ground: Ground): string => {
   return new Array(ground.dimensions.height)
     .fill(null)
     .map((_, rowNumber) => {
-      const playersInThisRow = ground.players.filter(({ position: { x } }) => rowNumber === x);
-      const foodInThisRow = ground.food.filter(({ position: { x } }) => rowNumber === x);
+      const predicateForRow = ({ position: { x } }: HasPosition): boolean => rowNumber === x;
+      const playersInThisRow = ground.players.filter(predicateForRow);
+      const foodInThisRow = ground.food.filter(predicateForRow);
       const row = new Array(ground.dimensions.width)
         .fill(null)
         .map((_, columnNumber) => {
-          const playerInSpot = playersInThisRow.find(({ position: { y } }) => columnNumber === y);
+          const predicateForSpot = ({ position: { y } }: HasPosition): boolean => columnNumber === y;
+          const playerInSpot = playersInThisRow.find(predicateForSpot);
           if (playerInSpot) {
             return ` ${playerInSpot.label} `;
           }
-          const foodInSpot = foodInThisRow.find(({ position: { y } }) => columnNumber === y);
+          const foodInSpot = foodInThisRow.find(predicateForSpot);
           if (foodInSpot) {
             return ` ${foodInSpot.label} `;
           }
