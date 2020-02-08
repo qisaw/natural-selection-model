39b7820874bcbcd23aa600383607cfd96e0e4be6
Wasiq Kashkari <w.kashkari@outlook.com>
2020-02-08 02:16:32 +0100

    Refactor test cases
    Re grouped the test cases so that it will make it easier to add more
    test cases later

--- src/player/can-reproduce.test.ts
+++ src/player/can-reproduce.test.ts
@@ -4,22 +4,14 @@ import { createGround } from '../ground/create-ground';
 import { canReproduce } from './can-reproduce';
 
 describe('canReproduce', () => {
-  describe('can reproduce cases', () => {
-    it('should return true if the player has eaten more than 2 food', () => {
-      const foodEaten = [createFood({ position: { x: 1, y: 1 } }), createFood({ position: { x: 1, y: 1 } })];
+  describe('player has eaten more than 2 food', () => {
+    it('should return true if there is a spot to put the new player', () => {
+      const foodEaten = [createFood({ position: { x: 0, y: 1 } }), createFood({ position: { x: 1, y: 1 } })];
       const player = createPlayer({ position: { x: 1, y: 1 }, foodEaten });
       const ground = createGround({ players: [player] });
       expect(canReproduce(player, ground)).toEqual(true);
     });
-  });
-  describe('can not reproduce cases', () => {
-    it('should return false if the player has eaten less than 2 food', () => {
-      const foodEaten = [createFood({ position: { x: 1, y: 1 } })];
-      const player = createPlayer({ position: { x: 1, y: 1 }, foodEaten });
-      const ground = createGround({ players: [player] });
-      expect(canReproduce(player, ground)).toEqual(false);
-    });
-    it('should return false if the user has eaten more than 2 food, but there is no available direction to reproduce in', () => {
+    it('should return false there is no available spot to put the new player', () => {
       const foodEaten = [createFood({ position: { x: 1, y: 1 } }), createFood({ position: { x: 1, y: 1 } })];
       const player = createPlayer({ position: { x: 1, y: 1 }, foodEaten });
       const otherPlayers = [
@@ -29,6 +21,14 @@ describe('canReproduce', () => {
         createPlayer({ position: { x: 1, y: 2 } }),
       ];
       const ground = createGround({ players: [player, ...otherPlayers] });
+      expect(canReproduce(player, ground)).toEqual(false);
+    });
+  });
+  describe('player has eaten less than 2 food', () => {
+    it('should return false if the player has eaten less than 2 food', () => {
+      const foodEaten = [createFood({ position: { x: 1, y: 1 } })];
+      const player = createPlayer({ position: { x: 1, y: 1 }, foodEaten });
+      const ground = createGround({ players: [player] });
       expect(canReproduce(player, ground)).toEqual(false);
     });
   });
