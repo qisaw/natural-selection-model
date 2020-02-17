1505135dc67f536ceacb104a930ee9817be85893
Wasiq Kashkari <w.kashkari@outlook.com>
2020-02-18 00:40:14 +0100

    Issue #18: Respect timeToLive.
    Time to live is the number of moves/turns a player can move for

--- src/ground/make-move.test.ts
+++ src/ground/make-move.test.ts
@@ -7,13 +7,13 @@ describe('makeMove', () => {
     width: 30,
     height: 30,
   };
-  const players = [
-    createPlayer({ position: { x: 0, y: 0 }, energy: 10 }),
-    createPlayer({ position: { x: 10, y: 10 }, energy: 10 }),
-    createPlayer({ position: { x: 20, y: 20 }, energy: 10 }),
-  ];
-  const ground = createGround({ dimensions, players });
   it('should move all players in a random manner', () => {
+    const players = [
+      createPlayer({ position: { x: 0, y: 0 }, energy: 10 }),
+      createPlayer({ position: { x: 10, y: 10 }, energy: 10 }),
+      createPlayer({ position: { x: 20, y: 20 }, energy: 10 }),
+    ];
+    const ground = createGround({ dimensions, players });
     //getNewPlayerPositionMock.mockReturnValue({ x: 1, y: 1 });
     const newGround = makeMove(ground);
     newGround.players.forEach(newPlayer => {
@@ -22,5 +22,17 @@ describe('makeMove', () => {
       expect(oldPlayer.position).toBeDefined();
       expect(oldPlayer.position).not.toEqual(newPlayer.position);
     });
+  });
+  it('should filter out all players with 0 timeToLive', () => {
+    const players = [createPlayer({ position: { x: 0, y: 0 }, energy: 10, timeToLive: 0 })];
+    const ground = createGround({ dimensions, players });
+    const newGround = makeMove(ground);
+    expect(newGround.players).toHaveLength(0);
+  });
+  it('should reduce player numbers by 1 on every call of makeMove', () => {
+    const players = [createPlayer({ position: { x: 0, y: 0 }, energy: 10, timeToLive: 1 })];
+    const ground = createGround({ dimensions, players });
+    const newGround = makeMove(ground);
+    expect(newGround.players[0].timeToLive).toEqual(0);
   });
 });
--- src/ground/make-move.ts
+++ src/ground/make-move.ts
@@ -1,12 +1,16 @@
 import { Ground } from './types';
 import { performAction } from '../player/perform-action';
 import { getPlayerMovementsForTurns } from './get-player-movements-for-turn';
+import { createPlayer } from '../player';
 
 export const makeMove = (ground: Ground): Ground => {
-  const { players } = ground;
-  let newGround = ground;
+  const { players: allPlayers } = ground;
+  const players = allPlayers.filter(({ timeToLive }) => timeToLive > 0);
+
+  let newGround = { ...ground, players };
   for (const [, playersToMove] of getPlayerMovementsForTurns(players)) {
     newGround = playersToMove.reduce((nextGround, player) => performAction(player, nextGround), ground);
   }
-  return newGround;
+  const finalPlayers = newGround.players.map(player => createPlayer({ ...player, timeToLive: player.timeToLive - 1 }));
+  return { ...newGround, players: finalPlayers };
 };
