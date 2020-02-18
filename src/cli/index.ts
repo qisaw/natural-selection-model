80d44f5f715696c0d83b30ba382f19dbd9e1cf4b
Wasiq Kashkari <w.kashkari@outlook.com>
2020-02-18 23:41:28 +0100

    Set version from package.json

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,10 +1,11 @@
 #! /usr/bin/env node
 import yargs from 'yargs';
 import { command } from './run-simulation';
+import { version } from '../../package.json';
 
 yargs
   .usage('$0 command')
   .command(command)
   .demandCommand()
   .wrap(80)
-  .version('0.0.1').argv;
+  .version(version).argv;
