b9e0754b9316a34a8fc6170cf43871421ac0ccaf
Wasiq Kashkari <w.kashkari@outlook.com>
2020-02-21 13:06:04 +0100

    Issue #13: Use getPositionFromDirection in getNewPlayerPosition

--- src/player/get-new-player-position.ts
+++ src/player/get-new-player-position.ts
@@ -2,10 +2,11 @@ import { Player } from './player';
 import { Position } from '../global/types';
 import { Ground } from '../ground/types';
 import { Direction } from '../global/direction';
+import { getPositionFromDirection } from '../utils/get-position-from-direction';
 
-const getRandomDirection = (availableDirections: Direction[]): Direction | void => {
+const getRandomDirection = (availableDirections: Direction[]): Direction => {
   if (!availableDirections.length) {
-    return;
+    return Direction.NO_DIRECTION;
   }
   const sparator = 1 / availableDirections.length;
   const randomValue = Math.random();
@@ -13,56 +14,8 @@ const getRandomDirection = (availableDirections: Direction[]): Direction | void 
   return availableDirections[index];
 };
 
-const getNextPosition = (xValue: number, yValue: number, direction: Direction): Position => {
-  if (direction === Direction.UP) {
-    return {
-      x: xValue,
-      y: yValue - 1,
-    };
-  }
-  if (direction === Direction.DOWN) {
-    return {
-      x: xValue,
-      y: yValue + 1,
-    };
-  }
-  if (direction === Direction.LEFT) {
-    return {
-      x: xValue - 1,
-      y: yValue,
-    };
-  }
-  if (direction === Direction.RIGHT) {
-    return {
-      x: xValue + 1,
-      y: yValue,
-    };
-  }
-  if (direction === Direction.UP_AND_LEFT) {
-    return {
-      x: xValue - 1,
-      y: yValue - 1,
-    };
-  }
-  if (direction === Direction.UP_AND_RIGHT) {
-    return {
-      x: xValue + 1,
-      y: yValue - 1,
-    };
-  }
-  if (direction === Direction.DOWN_AND_LEFT) {
-    return {
-      x: xValue - 1,
-      y: yValue + 1,
-    };
-  }
-  return {
-    x: xValue + 1,
-    y: yValue + 1,
-  };
-};
 const isPositionFree = (xValue: number, yValue: number, direction: Direction, ground: Ground): boolean => {
-  const nextPosition = getNextPosition(xValue, yValue, direction);
+  const nextPosition = getPositionFromDirection({ x: xValue, y: yValue }, direction);
   if (
     nextPosition.x > ground.dimensions.width - 1 ||
     nextPosition.x < 0 ||
@@ -77,7 +30,7 @@ const isPositionFree = (xValue: number, yValue: number, direction: Direction, gr
   return !playerAtPosition;
 };
 
-const getDirection = (player: Player, ground: Ground): Direction | void => {
+const getDirection = (player: Player, ground: Ground): Direction => {
   const canMoveUp = player.position.y > 0;
   const canMoveDown = player.position.y < ground.dimensions.height - 1;
   const canMoveLeft = player.position.x > 0;
@@ -96,7 +49,7 @@ const getDirection = (player: Player, ground: Ground): Direction | void => {
     ({ predicate, value }) => predicate && isPositionFree(player.position.x, player.position.y, value, ground),
   );
   const directionsPlayerHasYetToMoveTo = directionsAvailableToPlayer.filter(({ value }) => {
-    const possibleNextPosition = getNextPosition(player.position.x, player.position.y, value);
+    const possibleNextPosition = getPositionFromDirection(player.position, value);
     return !player.previousPositions.has(possibleNextPosition);
   });
   const availableDirections = directionsPlayerHasYetToMoveTo.length
@@ -108,50 +61,5 @@ const getDirection = (player: Player, ground: Ground): Direction | void => {
 export const getNewPlayerPosition = (player: Player, ground: Ground): Position => {
   // @TODO error checking for player in ground
   const direction = getDirection(player, ground);
-  switch (direction) {
-    case Direction.UP:
-      return {
-        x: player.position.x,
-        y: player.position.y - 1,
-      };
-    case Direction.DOWN:
-      return {
-        x: player.position.x,
-        y: player.position.y + 1,
-      };
-    case Direction.LEFT:
-      return {
-        x: player.position.x - 1,
-        y: player.position.y,
-      };
-    case Direction.RIGHT:
-      return {
-        x: player.position.x + 1,
-        y: player.position.y,
-      };
-    case Direction.DOWN_AND_LEFT:
-      return {
-        x: player.position.x - 1,
-        y: player.position.y + 1,
-      };
-    case Direction.DOWN_AND_RIGHT:
-      return {
-        x: player.position.x + 1,
-        y: player.position.y + 1,
-      };
-    case Direction.UP_AND_LEFT:
-      return {
-        x: player.position.x - 1,
-        y: player.position.y - 1,
-      };
-    case Direction.UP_AND_RIGHT:
-      return {
-        x: player.position.x + 1,
-        y: player.position.y - 1,
-      };
-    default:
-      return {
-        ...player.position,
-      };
-  }
+  return getPositionFromDirection(player.position, direction);
 };
