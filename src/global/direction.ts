397b1fe86644e777b9628b7f09c92ae35ca71bd5
Wasiq Kashkari <w.kashkari@outlook.com>
2020-02-21 10:33:29 +0100

    Issue #13: extract direction into it's own file
    Would like to use it in the getMovementDirectionViaSense function

---
+++ src/global/direction.ts
@@ -0,0 +1,10 @@
+export enum Direction {
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT,
+  UP_AND_LEFT,
+  UP_AND_RIGHT,
+  DOWN_AND_LEFT,
+  DOWN_AND_RIGHT,
+}
--- src/player/get-new-player-position.ts
+++ src/player/get-new-player-position.ts
@@ -1,17 +1,7 @@
 import { Player } from './player';
 import { Position } from '../global/types';
 import { Ground } from '../ground/types';
-
-enum Direction {
-  UP,
-  DOWN,
-  LEFT,
-  RIGHT,
-  UP_AND_LEFT,
-  UP_AND_RIGHT,
-  DOWN_AND_LEFT,
-  DOWN_AND_RIGHT,
-}
+import { Direction } from '../global/direction';
 
 const getRandomDirection = (availableDirections: Direction[]): Direction | void => {
   if (!availableDirections.length) {
