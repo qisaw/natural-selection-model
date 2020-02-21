7e8e485b989a0a9643e800b14ecea435723c8f9b
Wasiq Kashkari <w.kashkari@outlook.com>
2020-02-21 10:34:52 +0100

    Issue #13: Implement basic getMovementDirectionViaSense function

--- src/player/get-new-player-position-via-sense.test.ts
+++ src/player/get-new-player-position-via-sense.test.ts
@@ -1,7 +1,8 @@
 import { createPlayer } from '.';
 import { createGround } from '../ground/create-ground';
-import { getNewPlayerPositionViaSense } from './get-new-player-position-via-sense';
+import { getMovementDirectionViaSense } from './get-new-player-position-via-sense';
 import { createFood } from '../food';
+import { Direction } from '../global/direction';
 
 describe('getNewPlayerPositionViaSense', () => {
   describe('players with no sense', () => {
@@ -10,7 +11,7 @@ describe('getNewPlayerPositionViaSense', () => {
       const food = [createFood({ position: { x: 2, y: 2 } })];
       const dimensions = { width: 10, height: 10 };
       const ground = createGround({ dimensions, players: [playerWithNoSense], food });
-      expect(getNewPlayerPositionViaSense(playerWithNoSense, ground)).not.toBeDefined();
+      expect(getMovementDirectionViaSense(playerWithNoSense, ground)).not.toBeDefined();
     });
   });
   describe('players not in the board', () => {
@@ -19,7 +20,7 @@ describe('getNewPlayerPositionViaSense', () => {
       const dimensions = { width: 10, height: 10 };
       const food = [createFood({ position: { x: 2, y: 2 } })];
       const ground = createGround({ dimensions, players: [], food });
-      expect(getNewPlayerPositionViaSense(player, ground)).not.toBeDefined();
+      expect(getMovementDirectionViaSense(player, ground)).not.toBeDefined();
     });
   });
   describe('no food on the board', () => {
@@ -27,7 +28,61 @@ describe('getNewPlayerPositionViaSense', () => {
       const player = createPlayer({ position: { x: 1, y: 1 } });
       const dimensions = { width: 10, height: 10 };
       const ground = createGround({ dimensions, players: [player] });
-      expect(getNewPlayerPositionViaSense(player, ground)).not.toBeDefined();
+      expect(getMovementDirectionViaSense(player, ground)).not.toBeDefined();
+    });
+  });
+  describe('it can reach a piece of food this turn', () => {
+    it('should return the food position if the food if there is only one piece of food', () => {
+      const player = createPlayer({ position: { x: 1, y: 1 }, sense: 1 });
+      const dimensions = { width: 10, height: 10 };
+      const food = [createFood({ position: { x: 1, y: 2 } })];
+      const ground = createGround({ dimensions, players: [player], food });
+      expect(getMovementDirectionViaSense(player, ground)).toEqual(Direction.DOWN);
+      const foodToTheRight = [createFood({ position: { x: 2, y: 1 } })];
+      const groundWithFoodToTheRight = createGround({ dimensions, players: [player], food: foodToTheRight });
+      expect(getMovementDirectionViaSense(player, groundWithFoodToTheRight)).toEqual(Direction.RIGHT);
+      const foodToTheLeft = [createFood({ position: { x: 0, y: 1 } })];
+      const groundWithFoodToTheLeft = createGround({ dimensions, players: [player], food: foodToTheLeft });
+      expect(getMovementDirectionViaSense(player, groundWithFoodToTheLeft)).toEqual(Direction.LEFT);
+      const foodToTheTop = [createFood({ position: { x: 1, y: 0 } })];
+      const groundWithFoodToTheTop = createGround({ dimensions, players: [player], food: foodToTheTop });
+      expect(getMovementDirectionViaSense(player, groundWithFoodToTheTop)).toEqual(Direction.UP);
+      const foodToTheBottom = [createFood({ position: { x: 1, y: 2 } })];
+      const groundWithFoodToTheBottom = createGround({ dimensions, players: [player], food: foodToTheBottom });
+      expect(getMovementDirectionViaSense(player, groundWithFoodToTheBottom)).toEqual(Direction.DOWN);
+      const foodToTheBottomLeft = [createFood({ position: { x: 0, y: 2 } })];
+      const groundWithFoodToTheBottomLeft = createGround({ dimensions, players: [player], food: foodToTheBottomLeft });
+      expect(getMovementDirectionViaSense(player, groundWithFoodToTheBottomLeft)).toEqual(Direction.DOWN_AND_LEFT);
+      const foodToTheBottomRight = [createFood({ position: { x: 2, y: 2 } })];
+      const groundWithFoodToTheBottomRight = createGround({
+        dimensions,
+        players: [player],
+        food: foodToTheBottomRight,
+      });
+      expect(getMovementDirectionViaSense(player, groundWithFoodToTheBottomRight)).toEqual(Direction.DOWN_AND_RIGHT);
+      const foodToTheTopLeft = [createFood({ position: { x: 0, y: 0 } })];
+      const groundWithFoodToTheTopLeft = createGround({
+        dimensions,
+        players: [player],
+        food: foodToTheTopLeft,
+      });
+      expect(getMovementDirectionViaSense(player, groundWithFoodToTheTopLeft)).toEqual(Direction.UP_AND_LEFT);
+      const foodToTheTopRight = [createFood({ position: { x: 2, y: 0 } })];
+      const groundWithFoodToTheTopRight = createGround({
+        dimensions,
+        players: [player],
+        food: foodToTheTopRight,
+      });
+      expect(getMovementDirectionViaSense(player, groundWithFoodToTheTopRight)).toEqual(Direction.UP_AND_RIGHT);
+    });
+  });
+  describe('it cannot reach a piece of food this turn', () => {
+    it('should return undefined if the user cannot sense any food', () => {
+      const player = createPlayer({ position: { x: 1, y: 1 }, sense: 1 });
+      const dimensions = { width: 10, height: 10 };
+      const food = [createFood({ position: { x: 1, y: 3 } })];
+      const ground = createGround({ dimensions, players: [player], food });
+      expect(getMovementDirectionViaSense(player, ground)).toBeUndefined();
     });
   });
 });
--- src/player/get-new-player-position-via-sense.ts
+++ src/player/get-new-player-position-via-sense.ts
@@ -1,8 +1,37 @@
 import { Player } from './player';
 import { Ground } from '../ground/types';
+import { Direction } from '../global/direction';
 import { Position } from '../global/types';
 
-export const getNewPlayerPositionViaSense = (player: Player, ground: Ground): Position | void => {
+const getNextDirectionToTargetPosition = (currentPosition: Position, targetPosition: Position): Direction | void => {
+  if (currentPosition.x < targetPosition.x) {
+    if (currentPosition.y === targetPosition.y) {
+      return Direction.RIGHT;
+    }
+    if (currentPosition.y < targetPosition.y) {
+      return Direction.DOWN_AND_RIGHT;
+    }
+    return Direction.UP_AND_RIGHT;
+  }
+  if (currentPosition.x > targetPosition.x) {
+    if (currentPosition.y === targetPosition.y) {
+      return Direction.LEFT;
+    }
+    if (currentPosition.y < targetPosition.y) {
+      return Direction.DOWN_AND_LEFT;
+    }
+    return Direction.UP_AND_LEFT;
+  }
+
+  if (currentPosition.y < targetPosition.y) {
+    return Direction.DOWN;
+  }
+  if (currentPosition.y > targetPosition.y) {
+    return Direction.UP;
+  }
+};
+
+export const getMovementDirectionViaSense = (player: Player, ground: Ground): Direction | void => {
   if (player.sense === 0) {
     return;
   }
@@ -12,4 +41,13 @@ export const getNewPlayerPositionViaSense = (player: Player, ground: Ground): Po
   if (!ground.players.find(({ id }) => id === player.id)) {
     return;
   }
+  const closesetFoodWithinSense = ground.food.find(({ position: { x, y } }) => {
+    const isWithinXSense = x >= player.position.x - 1 && x <= player.position.x + 1;
+    const isWithinYSense = y >= player.position.y - 1 && y <= player.position.y + 1;
+    return isWithinXSense && isWithinYSense;
+  });
+  if (!closesetFoodWithinSense) {
+    return;
+  }
+  return getNextDirectionToTargetPosition(player.position, closesetFoodWithinSense.position);
 };
