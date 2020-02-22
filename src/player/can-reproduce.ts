bd0abe3f897aabb3a8361054e33bf5630745ff5b
Wasiq Kashkari <w.kashkari@outlook.com>
2020-02-22 03:21:31 +0100

    Use getFreePostionsAroundPlayer function to check if we can reproduce

--- src/player/can-reproduce.ts
+++ src/player/can-reproduce.ts
@@ -1,17 +1,11 @@
-import deepEqual from 'deep-equal';
-
-import { getNewPlayerPosition } from './get-new-player-position';
 import { Player } from './player';
 import { Ground } from '../ground/types';
 import { isOnOuterEdge } from '../ground/is-on-outer-edge';
+import { getFreePostionsAroundPlayer } from './get-free-positions-around-player';
 
 export const canReproduce = (player: Player, ground: Ground): boolean => {
   if (!isOnOuterEdge(player.position, ground)) {
     return false;
   }
-  // @TODO, just find an empty spot
-  if (deepEqual(getNewPlayerPosition(player, ground), player.position)) {
-    return false;
-  }
-  return player.foodEaten.length >= 2;
+  return player.foodEaten.length >= 2 && !!getFreePostionsAroundPlayer(player, ground, false).length;
 };
